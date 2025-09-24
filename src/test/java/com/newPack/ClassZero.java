e276317ef969fb4a7765448fe0bc23eb105aa733
gitHaq2531 <afzaulhaque1@gmail.com>
2025-09-24 15:53:07 +0530

    m5

--- src/test/java/com/newPack/ClassZero.java
+++ src/test/java/com/newPack/ClassZero.java
@@ -6,5 +6,6 @@ public static void main(String[] args) {
 	System.out.println("This is class zero ");
 	System.out.println("Updated by Harshali");
 	System.out.println("TL changes");
+	System.out.println("new change");
 	}
 }
