f48562b7713017f5a1d1db6348b6fdaaca826a41
DELL <DELL@DESKTOP-9060QJV>
2025-09-26 04:07:27 -0700

    One Class Has Been updated

---
+++ src/test/java/com/newPack/HelloVashi.java
@@ -0,0 +1,14 @@
+package com.newPack;
+
+public class HelloVashi {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("hello");
+		System.out.println("Wellcome");
+		System.out.println("vashi");
+
+	}
+
+}
