4b752a13c24508d3f8dd5409f0b34022b7d1859e
DELL <DELL@DESKTOP-9060QJV>
2025-09-30 05:34:58 -0700

    hi

--- src/test/java/com/newPack/SamiGitHub.java
+++ src/test/java/com/newPack/SamiGitHub.java
@@ -9,14 +9,10 @@ public class SamiGitHub {
 		System.out.println("rework by afshan");
 
 		System.out.println("updated one line again");
-<<<<<<< HEAD
-
-=======
 		System.out.println("hello");
 		System.out.println("bye");
 		System.out.println("TL changed");
 		System.out.println("newOne");
->>>>>>> branch 'master' of https://github.com/gitHaq2531/NMTPRepo.git
 	}
 	
 }
